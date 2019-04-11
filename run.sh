a489c13960e5c18b65b16c26ed90aa59bd428941
shengyushen <shengyushen@aliyun.com>
2019-04-11 07:13:53 +0000

    adding new

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 python /usr/share/models/official/mnist/mnist_tpu.py \
   --tpu=ssy-mtf-ctpu \
-  --data_dir=gs://ssystore1/data \
-  --model_dir=gs://ssystore1/output \
+  --data_dir=${DATADIR} \
+  --model_dir=${MODELDIR} \
   --use_tpu=True \
   --iterations=500 \
   --train_steps=2000
